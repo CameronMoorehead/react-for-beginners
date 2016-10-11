d02418f9fad5b8681ca9b35880629bf0587a1e85
Cameron <mooreheadc@hotmail.com>
2016-10-10 20:29:53 -0700

    updated

--- sandbox/src/components/App.js
+++ sandbox/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import Fish from './Fish'
 import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
         super()
         this.addFish = this.addFish.bind(this)
         this.loadSamples = this.loadSamples.bind(this)
+        this.addToOrder = this.addToOrder.bind(this)
         this.state = {
             fishes: {},
             order: {}
@@ -28,13 +30,26 @@ class App extends React.Component {
         })
     }
 
+    addToOrder(key) {
+        const order = {...this.state.order}
+        order[key] = order[key] + 1 || 1
+        this.setState({ order : order })
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="list-of-fishes">
+                        {
+                            Object
+                                .keys(this.state.fishes)
+                                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+                        }
+                    </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order} />
                 <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
